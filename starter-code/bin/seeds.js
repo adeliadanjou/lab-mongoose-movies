040f0e9420fd9d92561360cd2bd4e382855c8deb
Adelia d Anjou <princessadelia@gmail.com>
2019-08-24 05:25:57 +0200

    done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -36,12 +36,12 @@ const movies = [
       plot  : "Una mujer intenta salvar a su padre que queda atrapado en Florida durante un huracán de categoría 5. Durante su aventura, ambos se quedan atrapados en una casa que se inunda y que invaden una serie de salvajes depredadores."
     },
     {
-      name : "El rey león",
+      title : "El rey león",
       genre: "Drama",
       plot  : "Tras el asesinato de su padre, Simba, un joven león es apartado su reino y tendrá que descubrir con ayuda de amigos de la sabana africana el significado de la responsabilidad y la valentía. Más tarde volverá para recuperar el control de su reino."
     },
     {
-      name : "Aladdin",
+      title : "Aladdin",
       genre: "Fantasía",
       plot  : "Aladdin es un ladronzuelo que se enamora de la hija del Sultán, la princesa Jasmine. Para poder conquistarla aceptará un desafío de Jafar. Aladdín tendrá que entrar en una cueva en mitad del desierto y conseguir una lámpara mágica que contiene al Genio que será el encargado de concederle todos sus deseos"
     }
